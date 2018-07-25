765a28127c3cd3e1c83f22827744440b9dc850ba
Yann Lerjen <yann.lerjen@gmail.com>
2018-07-25 22:56:35 +0200

    Rename AuthorsRepository interface

--- Example.Repository/Repositories/AuthorsRepository.cs
+++ Example.Repository/Repositories/AuthorsRepository.cs
@@ -7,7 +7,6 @@ namespace Example.Repository.Repositories
 {
     public class AuthorsRepository : IAuthorsRepository
     {
-
         private readonly List<Author> authorList = new List<Author>() {
             new Author(new Guid("0ec5e125-5eee-4102-b48d-011a600fd74a"), "King", "Stephen", new DateTime()){ Descr = "Master of suspense" },
             new Author(new Guid("8f8cfba1-b30e-4289-a7ca-3aa122adb30a"), "Martin", "George RR", new DateTime()){ Descr = "An American novelist and short-story writer in the fantasy, horror, and science fiction genres" }
