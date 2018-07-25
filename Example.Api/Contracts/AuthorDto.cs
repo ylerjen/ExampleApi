a7c68c1a36364af9edf788cc27b1cc1dda6cf008
Yann Lerjen <yann.lerjen@gmail.com>
2018-07-25 23:24:13 +0200

    add basic validation

--- Example.Api/Contracts/AuthorDto.cs
+++ Example.Api/Contracts/AuthorDto.cs
@@ -1,18 +1,25 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace Example.Api.Models
 {
+    /// <summary>
+    /// The Author object used for data transfert.
+    /// Data validation is following the MS recommandation here <see cref="https://docs.microsoft.com/en-us/aspnet/core/tutorials/first-mvc-app/validation?view=aspnetcore-2.1"/>
+    /// </summary>
     public class AuthorDto
     {
         public Guid Id { get; set; }
 
+        [Required]
+        [StringLength(60, MinimumLength = 2)]
         public string Lastname { get; set; }
 
+        [Required]
+        [StringLength(60, MinimumLength = 2)]
         public string Firstname { get; set; }
 
+        [DataType(DataType.Date)]
         public DateTime Birthdate { get; set; }
     }
 }
