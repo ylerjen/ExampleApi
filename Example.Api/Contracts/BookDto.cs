57f404c4e2f8ebf742e152348f80aedaeef9b1f8
Yann Lerjen <yann.lerjen@gmail.com>
2018-07-25 22:57:19 +0200

    add book dto

---
+++ Example.Api/Contracts/BookDto.cs
@@ -0,0 +1,20 @@
+﻿using Example.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Example.Api.Contracts
+{
+    public class BookDto
+    {
+
+        public Guid Id { get; set; }
+
+        public string Title { get; set; }
+
+        public Guid AuthorId { get; set; }
+
+        public List<EBookCategory> CategoryList { get; set; }
+    }
+}
