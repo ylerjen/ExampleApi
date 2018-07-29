e885d2e29773d4670916530afad525ecc4f09da5
Yann Lerjen <yann.lerjen@gmail.com>
2018-07-29 20:15:36 +0200

    add swagger

--- Example.Api/Startup.cs
+++ Example.Api/Startup.cs
@@ -1,4 +1,5 @@
-﻿using Example.Domain.Entities;
+﻿using System;
+using Example.Domain.Entities;
 using Example.Business.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -9,11 +10,14 @@ using Example.Api.Commands;
 using Example.Api.Contracts;
 using Example.Helpers;
 using Example.Api.Middlewares;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace Example.Api
 {
     public class Startup
     {
+        private static readonly Version ApiVersion = new Version(1, 0, 0);
+
         public Startup(IConfiguration configuration)
         {
             this.Configuration = configuration;
@@ -32,6 +36,13 @@ namespace Example.Api
             services.AddMvc();
             services.AddScoped<IUsersService>(provider => new UsersService(new DateTimeProvider(), new UsersRepository()));
             services.AddScoped<IEventsService>(provider => new EventsService(new EventsRepository()));
+
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "Example API", Version = "v1" });
+                //c.SwaggerDoc("Edge", new Info { Title = "Example API", Version = ApiVersion.ToString() });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -51,6 +62,17 @@ namespace Example.Api
             {
                 cfg.CreateMap<User, UserDto>();
                 cfg.CreateMap<UserForCreationDto, User>();
+            });
+            
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), 
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Example API V1");
+               // c.RoutePrefix = string.Empty;
             });
 
             app.UseMvc();
